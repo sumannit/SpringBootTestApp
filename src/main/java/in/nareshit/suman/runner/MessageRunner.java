5b9adf68222d356f0bbe3e6701b83f23c52eff75
suman <suman@suman>
2020-08-18 16:55:57 +0530

    WH-4125 Update Runner With log using slf4j

--- src/main/java/in/nareshit/suman/runner/MessageRunner.java
+++ src/main/java/in/nareshit/suman/runner/MessageRunner.java
@@ -1,14 +1,26 @@
 package in.nareshit.suman.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
+	
 	@Override
 	public void run(String... args) throws Exception {
         int a = 10;
-		System.out.println("Welcome to Spring Boot App");
+        try {
+        	
+        	log.info("STARTED");
+        	System.out.println("Welcome to Spring Boot App");
+		} catch (Exception e) {
+         e.printStackTrace();
+		}
+	  log.info("END");
 	}
 }

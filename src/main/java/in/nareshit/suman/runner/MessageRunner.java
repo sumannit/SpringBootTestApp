1941463aff651d1f71dba06662205e391895d5d7
suman <suman@suman>
2020-08-18 16:22:52 +0530

    WH-554-abhi kiya hu update

--- src/main/java/in/nareshit/suman/runner/MessageRunner.java
+++ src/main/java/in/nareshit/suman/runner/MessageRunner.java
@@ -9,5 +9,6 @@ public class MessageRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Welcome to Spring Boot App");
+		System.out.println("Welcome to Spring Boot App");
 	}
 }

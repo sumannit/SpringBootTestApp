176490c2ca73f6d97ce508f751325440c2c42cec
suman <suman@suman>
2020-08-18 16:01:00 +0530

    WH-8520- First Time File Update

---
+++ src/main/java/in/nareshit/suman/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package in.nareshit.suman.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+       System.out.println("Welcome to Spring Boot App");
+	}
+
+}
